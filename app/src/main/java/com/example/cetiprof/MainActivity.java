33c62ccb9ecd41eabe8723c32840b5e65be54ece
Facosa99 <facosa99@gmail.com>
2020-06-10 21:26:09 -0500

    comentario

--- app/src/main/java/com/example/cetiprof/MainActivity.java
+++ app/src/main/java/com/example/cetiprof/MainActivity.java
@@ -11,6 +11,7 @@ import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
+    // MAU SE LA COME ENTERA
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
